ab80f24af5e9f756543e3f16493c9214389d54ff
marshall-GK <code.multiosop@gmail.com>
2024-09-05 11:34:06 +0530

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useContext, useEffect } from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+      </section>
+    </main>
+  );
+}
